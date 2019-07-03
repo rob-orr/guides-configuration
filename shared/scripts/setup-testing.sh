3b22013227a5108f0bf0493221731f693b7afac4
Rob Orr <rob.orr@appian.com>
2019-07-03 19:35:54 -0400

    fixed ruby and gem install problems

--- shared/scripts/setup-testing.sh
+++ shared/scripts/setup-testing.sh
@@ -2,7 +2,10 @@
 
 echo "Running"
 
-sudo gem install bundler --no-ri --no-rdoc
+source /home/ec2-user/.rvm/scripts/rvm
+rvm use 2.4
+ruby --version
+sudo gem install bundler --no-ri --no-rdoc -v '1.17.3'
 sudo /usr/local/bin/bundle install --system
 
 echo "Complete"
